07ea91d5417f8f7cf7baf3c2ad0ac7d1dd7dd74d
pengxy <pengxy@blizzmi.com>
2018-06-04 13:41:02 +0800

    fix some bugs

--- lib/xy_img.ex
+++ lib/xy_img.ex
@@ -3,69 +3,37 @@ defmodule XyImg do
   在phoenix `priv/static/images/xxx/目录`下生成图片.
   """
 
-  @expires Application.get_env(:xy_img, :expires) #图片存在时长
-  @phx_name Application.get_env(:xy_img, :phx_name) || :xy_img #phoenix项目名称
-  @path Application.get_env(:xy_img, :path) || "xy_img" #图片存放根目录
+  def expires, do: Application.get_env(:xy_img, :expires) #图片存在时长
+  def path, do: Application.get_env(:xy_img, :path, "images/xy_img") #static图片存放目录
 
 
   @doc """
   图片存放路径.
-
-  ## Examples
-
-      iex>  XyImg.save_path
-      :world
-
   """
   def save_path() do
-    appdir = Application.app_dir(@phx_name)
-    save_dir = "#{appdir}/priv/static/images/#{@path}"
+    save_dir = Path.join([File.cwd!(), "priv/static", path()])
     File.exists?(save_dir) || File.mkdir_p!(save_dir)
     save_dir
   end
 
   @doc """
   图片的物理路径
-
-  ## Examples
-
-      iex>  XyImg.file_url("xxx.png")
-      :world
-
   """
-  def file_path(filename), do: "#{save_path()}/#{filename}"
+  def file_path(filename), do: Path.join([save_path(), filename])
 
   @doc """
   图片的Phoenix.Endpoint.url路径
-
-  ## Examples
-
-      iex>  XyImg.file_url("xxx.png")
-      :world
-
   """
-  def file_url(filename, url \\ nil), do: "#{url}/images/#{@path}/#{filename}"
+  def file_url(filename, url \\ ""), do: Path.join([url, path(), filename])
 
 
   @doc """
   删除priv/static/images/xxx/xxx.png文件。
-
-  ## Examples
-
-      iex>  XyImg.del_file("xxx.png")
-      :world
-
   """
   def del_file(filename), do: File.rm(file_path(filename))
 
   @doc """
-  删除priv/static/images/xxx/所有文件。
-
-  ## Examples
-
-      iex>  XyImg.del_all()
-      :world
-
+  删除static/xxx目录。
   """
   def del_all() do
     File.rm_rf(save_path())
@@ -75,21 +43,15 @@ defmodule XyImg do
 
   @doc """
   创建图片。
-
-  ## Examples
-
-      iex>  XyImg.create_img("image/png","xxx")
-      :world
-
   """
   def create_img("image/jpeg", data_bin) do
-    do_create_img(@expires, data_bin, ".jpg")
+    do_create_img(expires(), data_bin, ".jpg")
   end
   def create_img("image/png", data_bin) do
-    do_create_img(@expires, data_bin, ".png")
+    do_create_img(expires(), data_bin, ".png")
   end
   def create_img("image/gif", data_bin) do
-    do_create_img(@expires, data_bin, ".gif")
+    do_create_img(expires(), data_bin, ".gif")
   end
   def create_img(_unknow_support_type, _data_bin) do
     {:error, :unknow_support_type}
